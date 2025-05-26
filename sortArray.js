6acf6b78818e09e5befab7eaf05ea6b15a9b96bb
Janeen-Inigo <inigojaneen01@gmail.com>
2025-05-26 23:17:18 +0800

    sortArray.js

--- sortArray.js
+++ sortArray.js
@@ -1,8 +1,8 @@
-let numbers = [24, 65, 21, 5, 9, 32, 42, 80, 57];
-let names = ["Zenvo", "Erica", "Jordie", "Alicia", "Redon"];
+let numbers = [24,65,21,5,9,32,42,80,57];
+let names = ["Zenvo","Erica","Jordie","Alicia","Redon"];
 
-let combined = [numbers, names];
-console.log("Combined array:", combined);
+let combined = [numbers,names];
+console.log("Combined array:",combined);
 
 // Sorting numbers in reverse order
 let sortedNumbers = [numbers].sort((a, b) => b - a);
